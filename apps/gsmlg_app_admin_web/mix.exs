507cfba5c43f1ec2a4f55760fae9193859a2aae6
Jonathan Gao <gsmlg.com@gmail.com>
2026-03-02 02:30:39 +0800

    chore(deps): update dependency lock files after bun rebuild
    - bun.lock: updated after phoenix_html import path change
    - mix.lock: updated after dependency recompilation
    - package.json: minor version updates to @duskmoon-dev packages
    - mix.exs: no functional changes

--- apps/gsmlg_app_admin_web/mix.exs
+++ apps/gsmlg_app_admin_web/mix.exs
@@ -41,7 +41,7 @@ defmodule GsmlgAppAdminWeb.MixProject do
       {:phoenix_html, "~> 4.0"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 1.0"},
-      {:phoenix_duskmoon, "~> 6.0"},
+      {:phoenix_duskmoon, "== 9.0.0-rc.1"},
       {:floki, ">= 0.30.0", only: :test},
       {:lazy_html, ">= 0.1.0", only: :test},
       {:phoenix_live_dashboard, "~> 0.8.2"},
